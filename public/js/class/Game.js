bc360daa9c30f8a8cd32bd9f758fc67a7e85f805
Sergey Reymerov <sergeyreym@gmail.com>
2014-04-12 13:04:59 +0900

    gathered all game classes into Game.js

--- public/js/class/Game.js
+++ public/js/class/Game.js
@@ -1,2 +1,46 @@
 Game = function() {
+    this.world = new Game.World();
+    this.player = new Game.Player();
+    this.world.players.push(this.player);
+}
+
+Game.World = function() {
+    this.players = [];
+    this.walls = [];
+    this.bombs = [];
+}
+
+Game.World.prototype.update = function(data) {
+
+}
+
+Game.Player = function() {
+    this.x = 0.0;
+    this.y = 0.0;
+    this.z = 0.0;
+}
+
+Game.Player.prototype.update = function(data) {
+    this.x = data.x;
+    this.y = data.y;
+    this.z = data.z;
+}
+
+Game.Player.prototype.move = function(keys) {
+    if (keys.left) {
+        this.x -= 0.1;
+    }
+    if (keys.right) {
+        this.x += 0.1;
+    }
+    if (keys.up) {
+        this.z += 0.1;
+    }
+    if (keys.down) {
+        this.z -= 0.1;
+    }
+}
+
+if (this.module) {
+    module.exports = Game;
 }
