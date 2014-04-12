2c49e3046a64b5c6540f89a5256e6c358a4e5b9a
Sergey Reymerov <sergeyreym@gmail.com>
2014-04-12 12:26:45 +0900

    Added class Keys.js and its logic to game.js.

--- public/js/game.js
+++ public/js/game.js
@@ -1,47 +1,48 @@
-/**
- * Created by sergejrejmerov on 12.04.14.
- */
-			var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+$(document).ready(function() {
+    var keys = new Keys(document);
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			document.body.appendChild(renderer.domElement);
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-			var geometry = new THREE.CubeGeometry(1,1,1);
-			var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-			var m2 = new THREE.MeshBasicMaterial({color: 0x0000FF});
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-			var cube = new THREE.Mesh(geometry, material);
+    var geometry = new THREE.CubeGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    var m2 = new THREE.MeshBasicMaterial({color: 0x0000FF});
 
-			var geometry1 = new THREE.Geometry();
+    var cube = new THREE.Mesh(geometry, material);
 
-geometry1.vertices.push( new THREE.Vector3(  -5,  0, -5 ) );
-geometry1.vertices.push( new THREE.Vector3(  -5, 0, 5 ) );
-geometry1.vertices.push( new THREE.Vector3(  5, 0, 5 ) );
-geometry1.vertices.push( new THREE.Vector3(  5, 0, -5 ) );
+    var geometry1 = new THREE.Geometry();
 
-			geometry1.faces.push( new THREE.Face3( 0, 1, 2 ) );
-			geometry1.faces.push( new THREE.Face3( 0, 2, 3 ) );
+    geometry1.vertices.push(new THREE.Vector3(-5, 0, -5));
+    geometry1.vertices.push(new THREE.Vector3(-5, 0, 5));
+    geometry1.vertices.push(new THREE.Vector3(5, 0, 5));
+    geometry1.vertices.push(new THREE.Vector3(5, 0, -5));
 
-			geometry1.computeBoundingSphere();
-			var z1 = new THREE.Mesh(geometry1, m2);
+    geometry1.faces.push(new THREE.Face3(0, 1, 2));
+    geometry1.faces.push(new THREE.Face3(0, 2, 3));
 
-			scene.add(cube);
-			scene.add(z1);
+    geometry1.computeBoundingSphere();
+    var z1 = new THREE.Mesh(geometry1, m2);
 
-			camera.position.z = 10;
-			camera.position.y = 5;
-			camera.position.x = 1;
-			camera.rotation.x -= 0.3; 
+    scene.add(cube);
+    scene.add(z1);
 
-			var render = function () {
-				requestAnimationFrame(render);
+    camera.position.z = 10;
+    camera.position.y = 5;
+    camera.position.x = 1;
+    camera.rotation.x -= 0.3;
 
-				//cube.rotation.x += 0.1;
-				//cube.rotation.y += 0.1;
+    var render = function () {
+        requestAnimationFrame(render);
 
-				renderer.render(scene, camera);
-			};
+        //cube.rotation.x += 0.1;
+        //cube.rotation.y += 0.1;
 
-			render();
+        renderer.render(scene, camera);
+    };
+
+    render();
+});
