55165e571b23119ebdbb1fe0f78507bcb6d2b3a5
lex <a.kazimirov@gmail.com>
2014-04-12 12:28:19 +0900

    Floor material changed

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,20 @@
 /**
  * Created by sergejrejmerov on 12.04.14.
  */
+
+
+			var materials = [];			
+			materials.push( new THREE.MeshLambertMaterial( { color: 0xdddddd, shading: THREE.FlatShading } ) );
+			materials.push( new THREE.MeshPhongMaterial( { ambient: 0x030303, color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } ) );
+			materials.push( new THREE.MeshNormalMaterial( ) );
+			materials.push( new THREE.MeshBasicMaterial( { color: 0xffaa00, transparent: true, blending: THREE.AdditiveBlending } ) );
+			materials.push( new THREE.MeshLambertMaterial( { color: 0xdddddd, shading: THREE.SmoothShading } ) );
+			materials.push( new THREE.MeshNormalMaterial( { shading: THREE.SmoothShading } ) );
+			materials.push( new THREE.MeshBasicMaterial( { color: 0xffaa00, wireframe: true } ) );
+			materials.push( new THREE.MeshDepthMaterial() );
+			materials.push( new THREE.MeshLambertMaterial( { color: 0x666666, emissive: 0xff0000, ambient: 0x000000, shading: THREE.SmoothShading } ) );
+			materials.push( new THREE.MeshPhongMaterial( { color: 0x000000, specular: 0x666666, emissive: 0xff0000, ambient: 0x000000, shininess: 10, shading: THREE.SmoothShading, opacity: 0.9, transparent: true } ) );
+			
 			var scene = new THREE.Scene();
 			var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
@@ -8,27 +22,27 @@
 			renderer.setSize(window.innerWidth, window.innerHeight);
 			document.body.appendChild(renderer.domElement);
 
-			var geometry = new THREE.CubeGeometry(1,1,1);
-			var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-			var m2 = new THREE.MeshBasicMaterial({color: 0x0000FF});
+			var player_geometry = new THREE.CubeGeometry(1,1,1);
+			var player_material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+			var player = new THREE.Mesh(player_geometry, player_material);
 
-			var cube = new THREE.Mesh(geometry, material);
+			var floor_material = materials[2];
+			var floor_geometry = new THREE.Geometry();
 
-			var geometry1 = new THREE.Geometry();
+			floor_geometry.vertices.push( new THREE.Vector3(  -5,  0, -5 ) );
+			floor_geometry.vertices.push( new THREE.Vector3(  -5, 0, 5 ) );
+			floor_geometry.vertices.push( new THREE.Vector3(  5, 0, 5 ) );
+			floor_geometry.vertices.push( new THREE.Vector3(  5, 0, -5 ) );
 
-geometry1.vertices.push( new THREE.Vector3(  -5,  0, -5 ) );
-geometry1.vertices.push( new THREE.Vector3(  -5, 0, 5 ) );
-geometry1.vertices.push( new THREE.Vector3(  5, 0, 5 ) );
-geometry1.vertices.push( new THREE.Vector3(  5, 0, -5 ) );
+			floor_geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
+			floor_geometry.faces.push( new THREE.Face3( 0, 2, 3 ) );
 
-			geometry1.faces.push( new THREE.Face3( 0, 1, 2 ) );
-			geometry1.faces.push( new THREE.Face3( 0, 2, 3 ) );
+			floor_geometry.computeBoundingSphere();
+			
+			var floorplane = new THREE.Mesh(floor_geometry, floor_material);
 
-			geometry1.computeBoundingSphere();
-			var z1 = new THREE.Mesh(geometry1, m2);
-
-			scene.add(cube);
-			scene.add(z1);
+			scene.add(player);
+			scene.add(floorplane);
 
 			camera.position.z = 10;
 			camera.position.y = 5;
